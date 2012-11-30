ebb064668c8519a8ef08644b601706e70e0aad36
Matthias Bolte <matthias@tinkerforge.com>
2012-11-30 18:52:31 +0100

    delphi: Remove unnecessary parentheses and call destructor again

--- software/examples/delphi/ExampleThreshold.pas
+++ software/examples/delphi/ExampleThreshold.pas
@@ -33,7 +33,7 @@ end;
 procedure TExample.Execute;
 begin
   { Create IP connection }
-  ipcon := TIPConnection.Create();
+  ipcon := TIPConnection.Create;
 
   { Create device object }
   v := TBrickletVoltage.Create(UID, ipcon);
@@ -53,6 +53,7 @@ begin
 
   WriteLn('Press key to exit');
   ReadLn;
+  ipcon.Destroy;
 end;
 
 begin
