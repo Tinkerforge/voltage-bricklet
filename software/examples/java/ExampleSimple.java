fefb31081ce3518575676a338474a0a62713e196
Matthias Bolte <matthias@tinkerforge.com>
2014-07-15 14:23:05 +0200

    java: Replace System.console() usage with an Eclipse compatible pattern

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -20,7 +20,7 @@ public class ExampleSimple {
 
 		System.out.println("Voltage: " + voltage/1000.0 + " V");
 
-		System.console().readLine("Press key to exit\n");
+		System.out.println("Press key to exit"); System.in.read();
 		ipcon.disconnect();
 	}
 }
