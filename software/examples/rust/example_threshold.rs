a53aa71d44f22ef56bd2e66b4bc81362546bd144
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:32:12 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Get threshold receivers with a debounce time of 10 seconds (10000ms).
     v.set_debounce_period(10000);
 
-    // Create receiver for voltage reached events.
-    let voltage_reached_receiver = v.get_voltage_reached_receiver();
+    let voltage_reached_receiver = v.get_voltage_reached_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `v` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `v` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for voltage_reached in voltage_reached_receiver {
