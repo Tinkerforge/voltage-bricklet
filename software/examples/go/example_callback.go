ad2a87ffd740c6ea112f65dc75ac82cf5a2edf1f
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:03:18 +0100

    go: Change import path.

--- software/examples/go/example_callback.go
+++ software/examples/go/example_callback.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/voltage_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/voltage_bricklet"
 )
 
 const ADDR string = "localhost:4223"
