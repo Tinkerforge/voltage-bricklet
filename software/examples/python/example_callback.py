262b855da5b8520576236500c51640be77eff68f
Erik Fleckstein <erik@tinkerforge.com>
2020-01-13 17:24:54 +0100

    examples: Use python3 input instead of python2 raw_input.

--- software/examples/python/example_callback.py
+++ software/examples/python/example_callback.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     #       if the voltage has changed since the last call!
     v.set_voltage_callback_period(1000)
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
